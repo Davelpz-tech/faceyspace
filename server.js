16d4bab4441b8ad6abed8ba25b367d4e023b5993
Davelpz-tech <davelpz15@gmail.com>
2022-07-22 22:12:11 -0700

    setting up server and video output on screen

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
-app.set(express.static('public'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
@@ -17,7 +17,8 @@ app.get('/:room', (req, res) => {
 
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-        console.log(roomId, userId)
+        socket.join(roomId)
+        socket.to(roomId).emit("user-connected", userId)
     })
 })
 
